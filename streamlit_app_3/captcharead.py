31a16c12ed9767e887f93a21030fc4ccc7f7c4e4
Anand-Karn <86818088+Anand-Karn@users.noreply.github.com>
2021-07-02 12:58:49 +0530

    Delete streamlit_app_3 directory

--- streamlit_app_3/captcharead.py
+++
@@ -1,9 +0,0 @@
-
-import pytesseract
-from PIL import Image
-img = Image.open(open("captcha_image.png", 'rb'))
-gray = img.convert('L')
-gray.save('captcha_gray.png')
-bw = gray.point(lambda x: 0 if x < 1 else 255, '1')
-bw.save('captcha_thresholded.png')
-print(pytesseract.image_to_string(bw))
